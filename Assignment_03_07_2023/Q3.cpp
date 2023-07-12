a967c63cc93f1c69f6edba65654674fcac204ccb
Nehal Garg <nehalgarg37@gmail.com>
2023-07-12 10:47:53 +0530

    ppt assignment

--- Assignment_03_07_2023/Q3.cpp
+++ Assignment_03_07_2023/Q3.cpp
@@ -10,3 +10,40 @@ You must solve the problem without using any built-in library for handling large
 **Output:**
 
 "134"*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+    if (num1.size() > num2.size())
+	return addStrings(num2, num1);
+    reverse(num1.begin(), num1.end());
+    reverse(num2.begin(), num2.end());
+    string sum;
+    int carry = 0;
+
+    for (int i=0; i < num1.size(); i++) {
+    	int curDigit = (num1[i] - '0' + num2[i] - '0' + carry) % 10;
+    	carry = (num1[i] - '0' + num2[i] - '0' + carry) / 10;
+    	sum += to_string(curDigit);
+    }
+    for (int i=0; i < num2.size(); i++) {
+    	int curDigit = (num2[i] - '0' + carry) % 10;
+    	carry = (num2[i] - '0' + carry) / 10;
+    	sum += to_string(curDigit);
+    }
+    if (carry == 1)
+    	sum += "1";
+    reverse(sum.begin(), sum.end());
+    return sum;
+}
+    
+};
+
+int main()
+{
+
+return 0;
+}
